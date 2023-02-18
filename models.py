4b72c80857a040e3169ea06e69cad3e180e0af11
OlgaTkachuk2022 <121062259+OlgaTkachuk2022@users.noreply.github.com>
2023-02-18 19:27:30 +0100

    Update models.py

--- models.py
+++ models.py
@@ -2,23 +2,10 @@ import json
 
 
 class Todos:
-  from flask import request
 
-@app.route("/api/v1/todos/", methods=["POST"])
-def create_todo():
-    if not request.json or not 'title' in request.json:
-      abort(400)
-todo = {
-    'id': todos.all()[-1]['id'] + 1,
-    'title': request.json['title'],
-    'description': request.json.get('description', ""),
-    'done': False
-  }
-todos.create(todo)
-
-def __init__(self):
+  def __init__(self):
     try:
-      with open("todos.json", "r") asf:
+      with open("todos.json", "r") as f:
         self.todos = json.load(f)
     except FileNotFoundError:
       self.todos = []
@@ -27,20 +14,15 @@ def all(self):
     return self.todos
 
 def get(self, id):
-    return self.todos[id]
+  todo = [todo for todo in self.all() if todo['id'] == id]
+  if todo:
+    return todo[0]
+  return []
 
 def create(self, data):
   self.todos.append(data)
   self.save_all()
 
-
-def delete(self, id):
-  todo = self.get(id)
-  if todo:
-    self.todos.remove(todo)
-    self.save_all()
-    return True
-  return False
 
 def save_all(self):
     with open("todos.json", "w") as f:
@@ -54,5 +36,14 @@ def update(self, id, data):
     self.save_all()
     return True
   return False
+
+def delete(self, id):
+  todo = self.get(id)
+  if todo:
+    self.todos.remove(todo)
+    self.save_all()
+    return True
+  return False
+
 
 todos = Todos()
